e89b849f423b52d6995209a2309de621aa4ec2c4
Vlad <vladislav.svyrydonov@ext.aperto.com>
2020-10-18 14:54:34 +0200

    new version of sidemap

--- util/generateSiteMap.js
+++ util/generateSiteMap.js
@@ -12,7 +12,6 @@ async function generateSiteMap() {
     ])
 
     const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
           ${pages
             .map(page => {
